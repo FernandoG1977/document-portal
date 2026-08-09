a9ace188289c77c30bfb44673ff972528ca6bb6b
FernandoG1977 <fernandogalindoreyes77@gmail.com>
2026-08-08 19:28:43 -0700

    Update Supabase anon key in config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 export const SUPABASE_URL = "https://yljeepydhpdqrggsborj.supabase.co";
-export const SUPABASE_ANON_KEY = "sb_publishable_TU_CLAVE_REAL";
+export const SUPABASE_ANON_KEY = "sb_publishable_yAcxklm1SdxBkG9vYhl-Vg_skRTJTRB";
 export const BUCKET = "documents";
